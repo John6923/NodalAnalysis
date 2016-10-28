2162b4103606e3a8fab417156470ba313eb1459e
John Dood <john.dood@hope.edu>
2016-10-28 10:24:00 -0400

    Add wire toString()

--- src/nodal/standard/Wire.java
+++ src/nodal/standard/Wire.java
@@ -13,5 +13,10 @@ public class Wire extends AbstractElement {
 	public void writeIVEquationToMatrix(Matrix m, int row, int iCol, int vCol) {
 		m.set(row, vCol, 1);
 	}
+	
+	@Override
+	public String toString() {
+		return "wire";
+	}
 
 }
